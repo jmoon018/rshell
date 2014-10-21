0c51732fd493a6018c219848494c142c6ac88b0f
Jamal Moon <jmoon018@ucr.edu>
2014-10-21 06:50:50 -0700

    Changes to exec.cpp. More comments, fixed tokenizer

--- exec.cpp
+++ exec.cpp
@@ -3,38 +3,62 @@
 #include <unistd.h>
 #include <errno.h>
 #include <stdlib.h>
-#include <string>
+#include <string.h>
 #include <iostream>
 
 using namespace std;
 
 bool printErrors = false;
-
+int totalWordCount = 0;
 void prnt(string s)
 {
 	if(printErrors)
 		cout << s << endl;
 }
 
-// responsible for splitting up the input into words
-char** tokenize(string input)
-{
-	vector<string>  v;
+/* 
+	char** tokenize(string)
+	===================================================================================
+	responsible for splitting up the input into words
+	creates a vector of strings (size of char** is undetermined until
+		parsing is done, so I use vectors because of their dynami size
+	
+	loops through all the letters of the input, and once a delimiter(space, etc)
+		is found, it will merge the letters into a word, add it to the vector,
+		and reset the word (word = "")
 
-	int cnt = 0, wordCnt = 0;
-	string word;	
+	when finished, a char** is made because the final size is finally known (v.size())
+		Loop through the vector and through each of its children and add its contents
+		to the new char**. 
+
+	return char** (cArray)
+	
+*/
+/*
+char**  tokenize(string input)
+{
+	vector<string>  v; // contains all the words delimited by spaces and ;
+
+	int cnt = 0, wordCnt = 0; // cnt=index in input string, wordCnt = current word length
+	string word; // add letters from input to this string, then add it to vector when delim. found	
+
 	for(string::iterator it=input.begin(); it != input.end(); it++)// not null
 	{
-		//cout << "Cnt: " << cnt << endl;
-		//cout << "swg" << endl;
-		if(input.at(cnt) == ' ')
+		if(input.at(cnt) == ' ') // delimiter #1 - space
 		{
 			prnt("Found a space");
-
+			
+			// add the word we have and reset it
 			v.push_back(word);	
 			word = "";
 
 			prnt("Delete worked fine");
+		}
+		else if(input.at(cnt) == ';') // delimiter #2 - semicolon
+		{
+			v.push_back(word); // push back w/e word we have
+			v.push_back(";"); // then add a semicolon [as a new 'word' or element in the vector]
+			word = ""; // reset word
 		}
 		else
 		{
@@ -46,9 +70,12 @@ char** tokenize(string input)
 			wordCnt++;	
 	}
 	
+	// add the last word (since it wasnt proccessed by the for loop above)
 	v.push_back(word);
 
+	// create char** and copy elements into it
    int	words = v.size();
+	totalWordCount = words;
 	char** cArray = new char*[words];
 
 	for(int i = 0; i < words; i++)
@@ -58,11 +85,58 @@ char** tokenize(string input)
 		{
 			curWord[b] = v.at(i).at(b);
 		}
+
+
 		cArray[i] = curWord;
 		//cout << "curword: " << v.at(i) << endl;
 	}
 
 	return cArray;
+}
+
+*/
+
+vector<string>  tokenize(string input)
+{
+	vector<string>  v; // contains all the words delimited by spaces and ;
+
+	int cnt = 0, wordCnt = 0; // cnt=index in input string, wordCnt = current word length
+	string word; // add letters from input to this string, then add it to vector when delim. found	
+
+	for(string::iterator it=input.begin(); it != input.end(); it++)// not null
+	{
+		if(input.at(cnt) == ' ') // delimiter #1 - space
+		{
+			prnt("Found a space");
+			
+			// add the word we have and reset it
+			v.push_back(word);	
+			word = "";
+			totalWordCount++;
+			prnt("Delete worked fine");
+		}
+		else if(input.at(cnt) == ';') // delimiter #2 - semicolon
+		{
+			v.push_back(word); // push back w/e word we have
+			v.push_back(";"); // then add a semicolon [as a new 'word' or element in the vector]
+			word = ""; // reset word
+			totalWordCount+= 2;
+		}
+		else
+		{
+			prnt("Found a normal character");
+
+		   word += input.at(wordCnt);	
+		}
+			cnt++;
+			wordCnt++;	
+	}
+	
+	// add the last word (since it wasnt proccessed by the for loop above)
+	v.push_back(word);
+	totalWordCount++;
+
+	return v;
 }
 
 
@@ -79,16 +153,28 @@ int main(int argc, char** argv)
 		// Wait for input
 		std::getline (std::cin, input);
 
-		char** words = tokenize(input);
+		vector<string> words = tokenize(input);
 		
-		//cout << "test..: " <<  *words << "..." << *(words+1) << endl;
-		
+		//for(int i = 0; i < totalWordCount; i++) { cout << words[i]  << " " << endl; }
+
+		//cout << "wordcnt: " << totalWordCount << endl;
+		char* h[totalWordCount];
+		for(int i = 0; i < totalWordCount; i++) 
+		{
+			h[i] = new char[words[i].size()+1];
+			copy(words[i].begin(), words[i].end(), h[i]);
+			h[i][words[i].size()] = '\0';
+		} 
+
+		h[totalWordCount] = NULL;
 		while(true) // break when we reach ;
 		{
-				int error = execvp(words[0], words);
+				cout << "swag" << endl;
+				int error = execvp(h[0], h);
 				if(error  == -1)
 				{
 					perror("ERROR");
+					exit(1);
 				}
 				else
 				{
@@ -97,6 +183,7 @@ int main(int argc, char** argv)
 				}
 				break;
 		}	
+		delete [] h; // free mem
 		break;
 	}
 	return 0;
